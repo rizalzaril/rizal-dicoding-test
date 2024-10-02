fcb354ccbd72a5177c58f90b1bc4d49ad79568e6
Rizal <zaril.ziral2020@gmail.com>
2024-10-03 04:03:20 +0700

    new

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const button = document.getElementById("toggleBtn");
+const biodataCard = document.getElementById("biodata-card");
+const eyeIcon = document.getElementById("eyeIcon");
+const contentCaption = document.querySelector("main .content .content-caption");
+
+// Initialize the visibility state
+let isVisible = false;
+
+button.addEventListener("click", () => {
+  isVisible = !isVisible;
+
+  if (isVisible) {
+    // Fade out and hide biodata
+    setTimeout(() => {
+      biodataCard.classList.add("visible");
+      button.textContent = "Tutup Biodata";
+    }, 100);
+  } else {
+    // Show biodata
+    setTimeout(() => {
+      biodataCard.classList.remove("visible");
+      button.textContent = "Lihat Biodata";
+    }, 100);
+  }
+
+  if (window.innerWidth > 768) {
+    if (contentCaption.style.width === "80%") {
+      contentCaption.style.opacity = "1"; // Fade out
+      setTimeout(() => {
+        contentCaption.style.width = "100%";
+        contentCaption.style.opacity = "1"; // Fade in
+      }, 100);
+    } else {
+      contentCaption.style.opacity = "1"; // Fade out
+      setTimeout(() => {
+        contentCaption.style.width = "80%";
+        contentCaption.style.opacity = "1"; // Fade in
+      }, 100);
+    }
+  }
+});
