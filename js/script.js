5e5f815a503f41f8045bcff04e8f9607fcdbc894
Rizal <zaril.ziral2020@gmail.com>
2024-10-07 06:16:05 +0700

    adasdasds

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 // Toggle menu
 
-// panggil id dari button html hamburger
+// Get the hamburger button element
 const menuToggle = document.getElementById("menuToggle");
 
-// panggil selector dari css
+// Get the menu element
 const menu = document.querySelector("header.nav-container .nav-list ul");
 
+// Add click event listener to the hamburger button
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 
-  // ganti icon saat menu tampil
+  // Change icon based on menu state
   if (menu.classList.contains("active")) {
     menuToggle.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   } else {
     menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  }
+});
+
+// Add scroll event listener to the window
+window.addEventListener("scroll", () => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active"); // Hide the menu
+    menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>'; // Change back to hamburger icon
   }
 });
